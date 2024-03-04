a33390d8816c63753cf9e226e88956757203f625
EndrioAlberton <96059097+EndrioAlberton@users.noreply.github.com>
2024-03-04 09:24:36 -0300

    Update about me

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -28,10 +28,7 @@ export function About() {
     >
       <h2>Sobre mim</h2>
       <p>
-      Tenho 19 anos, sou estudante de Sistemas para Internet no Instituto Federal do Rio Grande Do Sul, possuo diploma de técnico em Informática para Internet. 
-      Tenho  experiências em projetos acadêmicos e pessoais que podem ser visto em Projeto ou em meu GitHub, 
-      tenho paixão pela área de desenvolvimento, sinto uma enorme satisfação ao resolver problemas e aprender novas tecnologias. 
-      Busco oportunidades para ingressar na área.
+Tenho 20 anos, sou estudante de <b>Sistemas para Internet </b> no Instituto Federal do Rio Grande do Sul e possuo diploma de técnico em Informática para Internet. <b> Participei de projetos em grupo </b>, tanto no programa Nova Geração do Instituto Caldeira, como também no programa de bolsa de <b> Back-end com Spring Boot e ambiente cloud AWS </b>, pela Compass UOL. Minha paixão pela área de desenvolvimento é evidente, encontrando enorme satisfação em resolver problemas e absorver novas tecnologias. Estou empenhado em explorar novas oportunidades que me permitam aplicar minha paixão pelo desenvolvimento e contribuir de forma significativa para projetos desafiadores nessa área.
       </p>
     </AboutContainer>
   )
